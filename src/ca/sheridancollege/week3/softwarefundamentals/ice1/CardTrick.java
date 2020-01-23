d0dfe68a4ede93adf1523b786a3115ce0805dc65
tancalv <calvinwstan37@gmail.com>
2020-01-23 15:20:08 -0500

    Finished Card.java and CardTrick.java according to ICE1 details.

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +20,36 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue(c.genRandnum());
+            c.setSuit(Card.SUITS[c.genRandsuit()]);    
+            // Store each card object into the magicHand array
+            magicHand[i] = c;
+            // Print each magic hand to the screen.
+            System.out.println(c.getValue() + " of " + c.getSuit());
         }
         
         //insert code to ask the user for Card value and suit, create their card
         // and search magicHand here to find matching card is in array or not
         //Then report the result here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a value from 1 to 13:");
+        int value = sc.nextInt();
+        sc.nextLine();
+        System.out.println("Enter a suit (Diamonds, Clubs, Spades and Hearts:");
+        String suit = sc.nextLine();
+        boolean flag = false;
+        for (int j = 0; j < magicHand.length; j++){
+            if (magicHand[j].getValue() == value && 
+                    magicHand[j].getSuit().equalsIgnoreCase(suit)){
+                flag = true;
+            }
+        }
+        if (flag) {
+            System.out.println(true);
+        } else {
+            System.out.println(false);
+        }
+        
     }
     
 }
